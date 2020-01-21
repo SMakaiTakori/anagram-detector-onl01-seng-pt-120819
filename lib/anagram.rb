145d54873020ee947b112f36014d01659430aaed
S. Makai Takori <makait87@gmail.com>
2020-01-21 18:25:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,9 @@ class Anagram
   end  
   
   def match(words)
+    # iterate over the array of words that the .match method takes as an argument.
     words.select do |word|
+      
       @word.split("").sort == word.split("").sort
           
     end  
